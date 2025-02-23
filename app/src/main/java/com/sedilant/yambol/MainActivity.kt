1653bcc0a7ce5144eba278ed89166722ba68d8d1
Antonio Sediles <torosediles10@gmail.com>
2025-02-23 16:30:08 +0100

    Create NavigationBottomBar

--- app/src/main/java/com/sedilant/yambol/MainActivity.kt
+++ app/src/main/java/com/sedilant/yambol/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.sedilant.yambol.ui.NavigationBottomBar
+import com.sedilant.yambol.ui.home.HomeScreen
 import com.sedilant.yambol.ui.theme.YambolTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             YambolTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    NavigationBottomBar(modifier = Modifier.padding(innerPadding))
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YambolTheme {
-        Greeting("Android")
     }
 }
