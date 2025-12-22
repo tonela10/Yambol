a5fa572a342a2ef1e9e47911df122e4155df885a
torosediles <torosediles10@gmail.com>
2025-12-22 11:18:33 +0100

    Remove systemBarsPadding from CreateTrainScaffold and update YambolApp layout modifiers

--- app/src/main/java/com/sedilant/yambol/ui/createTrain/commonComposables/CreateTrainScaffold.kt
+++ app/src/main/java/com/sedilant/yambol/ui/createTrain/commonComposables/CreateTrainScaffold.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -37,7 +36,6 @@ fun CreateTrainScaffold(
     content: @Composable (PaddingValues) -> Unit
 ) {
     Scaffold(
-        modifier = Modifier.systemBarsPadding(),
         topBar = {
             Row(
                 modifier = Modifier
