5f09857bce0e9d26edacb4ba1da9f98be31b0a62
Antonio Sediles <torosediles10@gmail.com>
2026-03-09 16:53:25 +0100

    Refactor use case implementations to utilize auth state flow for user authentication

--- app/src/main/java/com/sedilant/yambol/domain/get/GetAllTasksUseCaseImpl.kt
+++ app/src/main/java/com/sedilant/yambol/domain/get/GetAllTasksUseCaseImpl.kt
@@ -3,27 +3,35 @@ package com.sedilant.yambol.domain.get
 import com.sedilant.yambol.data.firebaseAuth.AuthRepository
 import com.sedilant.yambol.data.firestore.TaskRepository
 import com.sedilant.yambol.domain.models.TaskDomain
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class GetAllTasksUseCaseImpl @Inject constructor(
     private val taskRepository: TaskRepository,
     private val authRepository: AuthRepository
 ) : GetAllTaskUseCase {
     override fun invoke(): Flow<List<TaskDomain>> {
-        val userId = authRepository.currentUser?.uid ?: return flowOf(emptyList())
-
-        return taskRepository.listByUserFlow(userId).map { list ->
-            list.map {
-                TaskDomain(
-                    trainingTaskId = it.id,
-                    name = it.name,
-                    concepts = it.conceptIds,
-                    description = it.description,
-                    variables = it.variables
-                )
+        return authRepository.getAuthStateFlow().flatMapLatest { user ->
+            val userId = user?.uid
+            if (userId == null) {
+                flowOf(emptyList())
+            } else {
+                taskRepository.listByUserFlow(userId).map { list ->
+                    list.map {
+                        TaskDomain(
+                            trainingTaskId = it.id,
+                            name = it.name,
+                            concepts = it.conceptIds,
+                            description = it.description,
+                            variables = it.variables
+                        )
+                    }
+                }
             }
         }
     }
